cf8da582030501fe2ab7f7849f7aedea389eaa47
Andrew Summers <18727110+summersab@users.noreply.github.com>
2023-08-01 14:09:57 -0500

    fix CORS 2

--- index.js
+++ index.js
@@ -61,9 +61,7 @@ const corsOptions = {
 		// (like mobile apps or curl requests)
 		if(!origin) return callback(null, true);
 		if(allowedOrigins.indexOf(origin) === -1){
-			var msg = 'The CORS policy for this site does not ' +
-				'allow access from the specified Origin.';
-			return callback(new Error(msg), false);
+			return callback(null, false);
 		}
 		return callback(null, true);
 	},
